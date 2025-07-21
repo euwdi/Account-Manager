a84fc7132953962a3d65c52158decd02d848aecd
eugen1337 <evgenijkurakin478@gmail.com>
2025-07-21 17:37:16 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>Удобно управляйте учетными записями!</h1>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.container {
+  display: flex;
+}
+</style>
